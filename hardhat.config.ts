139cadcd38bfdd3f5d91c4d555823266ef0775c0
ikmz <ikmz0104.official@gmail.com>
2023-12-10 15:41:10 +0900

    Optimize code size in Solidity compiler

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,16 @@ require("dotenv").config();
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      // コードサイズを最適化
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   networks: {
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
