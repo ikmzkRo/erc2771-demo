d57f5467f52f64eaba000dea4b80cddd73f8f492
ikmz <ikmz0104.official@gmail.com>
2023-12-10 18:11:49 +0900

    Update ERC721Factory deployment script and handle transaction error

--- scripts/depoloy/factory/erc721Factory.ts
+++ scripts/depoloy/factory/erc721Factory.ts
@@ -1,9 +1,8 @@
-import * as hre from "hardhat";
+import { ethers } from "hardhat";
 
 async function main() {
-  await hre.run("compile");
 
-  const IkmzERC721Factory = await hre.ethers.getContractFactory(
+  const IkmzERC721Factory = await ethers.getContractFactory(
     "IkmzERC721Factory"
   );
   const ikmzERC721Factory = await IkmzERC721Factory.deploy();
@@ -11,14 +10,18 @@ async function main() {
 
   console.log("IkmzERC721Factory deployed to:", `https://goerli.etherscan.io/address/${ikmzERC721Factory.address}`);
 
-  const name = "kicr"
-  const symbol = "ikmz"
+  const name = "testName"
+  const symbol = "testSymbol"
   const baseTokenURI = "https://aws/s3_bucket/honyohonyo"
   const admin = "0xa2fb2553e57436b455F57270Cc6f56f6dacDA1a5"
   const trustedForwarder = "0xBd1471fD728dd7e934141a581eC74CEba50FDB16"
 
   let tx = await ikmzERC721Factory.createIkmzERC721(name, symbol, baseTokenURI, admin, trustedForwarder);
-  await tx.wait();
+  try {
+    await tx.wait();
+  } catch (error) {
+    console.log('error', error)
+  }
 
 }
 
