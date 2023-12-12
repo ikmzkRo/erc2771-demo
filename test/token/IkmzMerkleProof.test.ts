3abe7a738e1b5e9b63e36d52086fdf49a9e91c62
ikmz <ikmz0104.official@gmail.com>
2023-12-12 18:10:41 +0900

    Add new dependencies and implement tests for IkmzMerkleProof contract

--- test/token/IkmzMerkleProof.test.ts
+++ test/token/IkmzMerkleProof.test.ts
@@ -1,22 +1,46 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 import { Contract } from "ethers";
+import chai from "chai";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import ChaiAsPromised from "chai-as-promised";
+import keccak256 from "keccak256";
+import { MerkleTree } from "merkletreejs";
+// TODO: typechain install
+// import type { IkmzMerkleProof } from "../typechain-types";
 
-describe("IkmzMerkleProof", function () {
+// 通常、Chaiは同期的なテストのために使用されますが、Ethereumのスマートコントラクトのテストのように非同期な処理を含む場合、ChaiAsPromised プラグインが役立ちます。
+chai.use(ChaiAsPromised);
+
+describe("IkmzMerkleProof", async function () {
   const name = "testName";
   const symbol = "testSymbol";
   let IkmzMerkleProof: any;
   let ikmzMerkleProof: Contract;
   let owner: SignerWithAddress;
   let to: SignerWithAddress;
+  let allowListedUser: SignerWithAddress;
+  let notListedUser: SignerWithAddress;
 
   beforeEach(async function () {
-    [owner, to] = await ethers.getSigners();
+    [owner, to, allowListedUser, notListedUser] = await ethers.getSigners();
     IkmzMerkleProof = await ethers.getContractFactory("IkmzMerkleProof");
     ikmzMerkleProof = await IkmzMerkleProof.deploy(name, symbol);
     await ikmzMerkleProof.deployed();
   })
+
+  const allowList = [allowListedUser.address];
+
+  const merkleTree = new MerkleTree(allowList.map(keccak256), keccak256, {
+    sortPairs: true,
+  });
+  const hexProof = merkleTree.getHexProof(keccak256(allowListedUser.address));
+  const rootHash = merkleTree.getRoot();
+
+  await ikmzMerkleProof
+    .connect(owner)
+    .setMerkleRoot(`0x${rootHash.toString("hex")}`);
 
   it("[S] The deployment address should be set as the owner.", async function () {
     expect(await ikmzMerkleProof.owner()).to.equal(owner.address)
@@ -25,5 +49,23 @@ describe("IkmzMerkleProof", function () {
   it("[S} The owner should be able to create NFTs.", async function () {
     await ikmzMerkleProof.mint(to.address)
     expect(await ikmzMerkleProof.ownerOf(1)).to.equal(to.address)
+  });
+
+  // setMerkleRoot は owner のみ設定可能
+  it("[S] The test for setMerkleRoot being onlyOwner.", async function () {
+    await expect(
+      ikmzMerkleProof
+        .connect(notListedUser)
+        .setMerkleRoot(`0x${rootHash.toString("hex")}`)
+    ).to.be.revertedWith("Ownable: caller is not the owner");
+  });
+
+  it("[S] Check balance", async function () {
+    expect(await ikmzMerkleProof.balanceOf(allowListedUser.address)).to.be.equal(
+      BigInt(0)
+    );
+    expect(await ikmzMerkleProof.balanceOf(notListedUser.address)).to.be.equal(
+      BigInt(0)
+    );
   });
 });
