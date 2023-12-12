9267bb9b06b006f8966c8218443f6c8c809c60ca
ikmz <ikmz0104.official@gmail.com>
2023-12-12 20:49:49 +0900

    Add Merkle Proof functionality and tests

--- test/token/IkmzMerkleProof.test.ts
+++ test/token/IkmzMerkleProof.test.ts
@@ -22,25 +22,32 @@ describe("IkmzMerkleProof", async function () {
   let to: SignerWithAddress;
   let allowListedUser: SignerWithAddress;
   let notListedUser: SignerWithAddress;
+  let hexProof: string[];
+  let rootHash: Buffer;
 
   beforeEach(async function () {
     [owner, to, allowListedUser, notListedUser] = await ethers.getSigners();
+
+    // マークルツリーを構築します
+    // TODO: アドレス個別に発行する数を指定してみる
+    const allowList = [allowListedUser.address];
+    const merkleTree = new MerkleTree(allowList.map(keccak256), keccak256, {
+      sortPairs: true,
+    });
+    // コントラクトが AllowList にあるかどうかを計算
+    hexProof = merkleTree.getHexProof(keccak256(allowListedUser.address));
+    // ツリーの最上を算出
+    rootHash = merkleTree.getRoot();
+    // コントラクトにマークルルートを登録する
+    await ikmzMerkleProof
+      .connect(owner)
+      .setMerkleRoot(`0x${rootHash.toString("hex")}`);
+
     IkmzMerkleProof = await ethers.getContractFactory("IkmzMerkleProof");
     ikmzMerkleProof = await IkmzMerkleProof.deploy(name, symbol);
     await ikmzMerkleProof.deployed();
   })
-
-  const allowList = [allowListedUser.address];
-
-  const merkleTree = new MerkleTree(allowList.map(keccak256), keccak256, {
-    sortPairs: true,
-  });
-  const hexProof = merkleTree.getHexProof(keccak256(allowListedUser.address));
-  const rootHash = merkleTree.getRoot();
-
-  await ikmzMerkleProof
-    .connect(owner)
-    .setMerkleRoot(`0x${rootHash.toString("hex")}`);
+  console.log('(; ･`д･´)')
 
   it("[S] The deployment address should be set as the owner.", async function () {
     expect(await ikmzMerkleProof.owner()).to.equal(owner.address)
@@ -60,12 +67,29 @@ describe("IkmzMerkleProof", async function () {
     ).to.be.revertedWith("Ownable: caller is not the owner");
   });
 
-  it("[S] Check balance", async function () {
+  it("[S] Check the current balance", async function () {
     expect(await ikmzMerkleProof.balanceOf(allowListedUser.address)).to.be.equal(
       BigInt(0)
     );
     expect(await ikmzMerkleProof.balanceOf(notListedUser.address)).to.be.equal(
       BigInt(0)
     );
+  });
+
+  it("[S] mint by allowListedUserAddress", async function () {
+    await ikmzMerkleProof.connect(allowListedUser).mint(hexProof);
+  });
+
+  it("[S] mint by notListedUserAddress", async function () {
+    await expect(ikmzMerkleProof.connect(notListedUser).mint(hexProof)).to.be.revertedWith(
+      "Invalid proof"
+    );
+  });
+
+  it("[S] Check the current balance", async function () {
+    expect(await ikmzMerkleProof.balanceOf(allowListedUser.address)).to.be.equal(
+      BigInt(1)
+    );
+    expect(await ikmzMerkleProof.balanceOf(notListedUser.address)).to.be.equal(BigInt(0));
   });
 });
