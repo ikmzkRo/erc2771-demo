16bd44fb20fea01f4dad5d411bc07b3a7edf5fcf
ikmz <ikmz0104.official@gmail.com>
2023-12-12 17:51:10 +0900

    Add merkleRoot functionality to IkmzMerkleProof contract

--- contracts/token/ikmz-ERC721/IkmzMerkleProof.sol
+++ contracts/token/ikmz-ERC721/IkmzMerkleProof.sol
@@ -5,12 +5,12 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract IkmzMerkleProof is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdTracker;
-
-    string private _baseTokenURI;
+    bytes32 public merkleRoot;
 
     constructor(
       string memory name,
@@ -24,5 +24,9 @@ contract IkmzMerkleProof is ERC721URIStorage, Ownable {
         _mint(_to, tokenId);
         _tokenIdTracker.increment();
         return tokenId;
+    }
+
+    function setMerkleRoot(bytes32 _merkleRoot) public onlyOwner {
+        merkleRoot = _merkleRoot;
     }
 }
